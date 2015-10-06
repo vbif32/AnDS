a8b3ac449ccaa77e97058314419604cd0e4247b2
Dartaan <vbif.32@mail.ru>
2015-10-07 02:51:00 +0300

    Start

---
+++ Lab1-graph/src/Graph.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Dartaan on 06.10.2015.
+ */
+
+public class Graph {
+
+    private class GraphNode {
+        int id;  // номер вершины
+        ArrayList<Integer> neighbours; // список соседей
+    }
+
+
+
+}
